3839fad8114a01b6398127a466ca75bf8687a526
Michael Barnett <micalan@sas.upenn.edu>
2017-03-19 23:34:11 -0400

    Code to break up images in patches

--- makePatches.m
+++ makePatches.m
@@ -1,2 +1,18 @@
-function [] = makePatches(imStack,patchSize)
+function [Mr] = makePatches(imStack,patchSize)
 
+% function to break up an image into many patches of any given size
+numRows = size(imStack,1);
+numCols = size(imStack,2);
+numImgs = size(imStack,3);
+
+idx = 0;
+for i = 1:size(imStack,3)
+    Im = imStack(:,:,i);
+    ii=1:patchSize:numRows;
+    [a,b]=ndgrid(ii(1:end-1),ii(1:end-1));
+    M=arrayfun(@(x,y) Im(x:x+patchSize-1,y:y+patchSize-1),a,b,'un',0);
+    zDim = size(M,1)*size(M,2);
+    Mr(:,:,idx+1:idx+zDim) = cell2mat(reshape(M,1,1,size(M,1)*size(M,2)));
+    idx = idx +zDim;
+    
+end
