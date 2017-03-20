d1aeaebba0fcbe63d9f6e9446a27ac811ab92b3a
Michael Barnett <micalan@sas.upenn.edu>
2017-03-19 23:41:56 -0400

    Added master run script

--- makePatches.m
+++ makePatches.m
@@ -1,4 +1,4 @@
-function [Mr] = makePatches(imStack,patchSize)
+function [imPatches] = makePatches(imStack,patchSize)
 
 % function to break up an image into many patches of any given size
 numRows = size(imStack,1);
@@ -12,7 +12,7 @@ for i = 1:size(imStack,3)
     [a,b]=ndgrid(ii(1:end-1),ii(1:end-1));
     M=arrayfun(@(x,y) Im(x:x+patchSize-1,y:y+patchSize-1),a,b,'un',0);
     zDim = size(M,1)*size(M,2);
-    Mr(:,:,idx+1:idx+zDim) = cell2mat(reshape(M,1,1,size(M,1)*size(M,2)));
+    imPatches(:,:,idx+1:idx+zDim) = cell2mat(reshape(M,1,1,size(M,1)*size(M,2)));
     idx = idx +zDim;
     
 end
