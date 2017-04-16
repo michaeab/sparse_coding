9ac57f2b78c6c465ed2a62aa91f1dba05be5c634
benchinny <ben.chinny@gmail.com>
2017-04-16 15:20:43 -0400

    Delete equationFive.m

--- equationFive.m
+++
@@ -1,9 +0,0 @@
-function aDot = equationFive(I,phi,a,lambda,sigma)
-
-% function aDot = equationFive(I,phi,a,lambda,sigma)
-%
-% implements all of equation 5 of Olshausen and Field paper
-
-aDot = biFunc(phi,I) - sumCaFunc(phi,a) - (lambda./sigma).*Sderivative(a./sigma);
-
-end
